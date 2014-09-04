23446026b9ccd1308ae9de4d5c52ab829e4ccd03
chenglch <chenglch@cn.ibm.com>
2014-09-03 23:05:32 -0400

    modify console_log.sh so support gerrit log pattern

--- ironic-xcat-test/console_log.sh
+++ ironic-xcat-test/console_log.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 source env.sh
-DATE_DIR=`date +%Y-%m-%d`
-function job_log {
-    mkdir -p /tmp/ironic-xcat-test/logs/$DATE_DIR
-    cd /tmp/ironic-xcat-test/logs/$DATE_DIR
+#DATE_DIR=`date +%Y-%m-%d`
+function create_log_dir {
+    mkdir -p /tmp/ironic-xcat-test/logs/
+    cd /tmp/ironic-xcat-test/logs/
     if [ -z "$ZUUL_CHANGE" ]; then
         export ZUUL_CHANGE=0
     fi
     if [ -z "$ZUUL_PIPELINE" ]; then
         export ZUUL_PIPELINE="check"
     fi
-    export ZUUL_LOG_DIR=zuul_${ZUUL_CHANGE}_${JOB_NAME}_${BUILD_NUMBER}
-    mkdir $ZUUL_LOG_DIR
-    scp -i /opt/ci_tmp/id_rsa -r jenkins@testcislave-tmp:/opt/stack/logs $ZUUL_LOG_DIR
+    if [ -z "$ZUUL_PATCHSET" ]; then
+        export ZUUL_PATCHSET=0
+    fi
+    export ZUUL_LOG_DIR=${ZUUL_CHANGE}/${ZUUL_PATCHSET}/${ZUUL_PIPELINE}/${JOB_NAME}/${BUILD_NUMBER}
+    mkdir -p $ZUUL_LOG_DIR
 }
 
-job_log
-cd /tmp/ironic-xcat-test/logs/$DATE_DIR/$ZUUL_LOG_DIR
-
-
+create_log_dir
+# copy devstack setup log
+scp -i /opt/ci_tmp/id_rsa -r jenkins@testcislave-tmp:/opt/stack/logs $ZUUL_LOG_DIR
+cd /tmp/ironic-xcat-test/logs/$ZUUL_LOG_DIR
 wget http://testcimaster:8080/job/$JOB_NAME/$BUILD_NUMBER/consoleText
 mv consoleText console.log
+
 if [[ "$ZUUL_CHANGE" != 0 ]]; then
-    scp -i /opt/ci_tmp/sourceforge/id_rsa -r /tmp/ironic-xcat-test/logs/$DATE_DIR chenglch,xCAT@web.sourceforge.net:/home/frs/project/xcat/OpenStack/CI
+    scp -i /opt/ci_tmp/sourceforge/id_rsa -r /tmp/ironic-xcat-test/logs/$ZUUL_CHANGE chenglch,xCAT@web.sourceforge.net:/home/frs/project/xcat/OpenStack/CI
 fi
-
-#ssh chenglch,xCAT@web.sourceforge.net mkdir -p /home/frs/project/xcat/OpenStack/CI/$DATE_DIR
-
-#scp -i /opt/ci_tmp/id_rsa -r 2014-08-31 jenkins@9.114.34.161:/tmp
-#scp -i /opt/ci_tmp/id_rsa -r jenkins@9.114.34.161:/opt/stack/logs /tmp
